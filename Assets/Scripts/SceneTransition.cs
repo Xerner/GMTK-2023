1ced367340ee77175885cda4e6c88a176721e6dd
Kenneth Mead <kennethmead784@gmail.com>
2023-07-08 18:12:25 -0400

    Added pause menu

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -4,15 +4,6 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string SceneName;
-    public bool AutoSubscribe = true;
-
-    private void Awake()
-    {
-        if (AutoSubscribe)
-        {
-            //transform.find
-        }
-    }
 
     public void GoToScene()
     {
--- Assets/Scripts/UI/TextSync.cs
+++ Assets/Scripts/UI/TextSync.cs
@@ -1,39 +1,88 @@
 using EasyButtons;
+using MarkupAttributes;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-[AddComponentMenu("UI/Text Sync")]
-[RequireComponent(typeof(TMPro.TextMeshProUGUI))]
-public class TextSync : MonoBehaviour {
-    [Description("Defaults to this GameObjects TextMeshPro (if available)")]
-    public TMPro.TextMeshProUGUI sourceText;
+public class TextSync : MonoBehaviour
+{
+    public ESource sourceType;
+    private bool UsingTextMeshPro => sourceType == ESource.TextMeshPro;
+    private bool UsingSlider => sourceType == ESource.Slider;
+
+    private MonoBehaviour source;
+    
+    public List<TMPro.TextMeshProUGUI> textsToSync;
+
+
+    [Header("TextMeshPro")]
+    public TMPro.TextMeshProUGUI textSource;
     public bool syncText = true;
     public bool syncFontSize = true;
     [Description(@"Syncs the text values between two Text Mesh Pro components
     
 If empty, defaults to the first childs Text Mesh Pro component (if it has one)")]
     public bool syncMargin = true;
-    [Space()]
-    public List<TMPro.TextMeshProUGUI> textsToSync;
+    [EndGroup]
 
-    void OnValidate() {
-        Sync();
+    [Header("Slider")]
+    public Slider sliderSource;
+
+    void OnValidate()
+    {
+        switch (sourceType)
+        {
+            case ESource.Slider:
+                sliderSource = GetComponent<Slider>();
+                source = sliderSource;
+                break;
+            default:
+                textSource = GetComponent<TMPro.TextMeshProUGUI>();
+                source = textSource;
+                break;
+        }
+        if (source == null) Debug.LogError("No valid text source found. Please provide one");
+        SyncAll();
     }
 
-    void Update() {
-        Sync();
+    void Update()
+    {
+        SyncAll();
     }
 
     [Button]
-    public void Sync() {
-        if (sourceText == null) {
-            if (sourceText == null) sourceText = GetComponent<TMPro.TextMeshProUGUI>();
-            if (sourceText == null) Debug.LogError("No TextMeshProUGUI provided or found");
+    public void SyncAll()
+    {
+        if (source == null) return;
+        foreach (TMPro.TextMeshProUGUI textToSync in textsToSync)
+        {
+            Sync(textToSync);
         }
-        foreach (TMPro.TextMeshProUGUI text in textsToSync) {
-            if (syncText) text.text = sourceText.text;
-            if (syncFontSize) text.fontSize = sourceText.fontSize;
-            if (syncMargin) text.margin = sourceText.margin;
+    }
+
+    public void Sync(TMPro.TextMeshProUGUI textToSync)
+    {
+        if (source == null) return;
+        if (textToSync == null) return;
+        switch (sourceType)
+        {
+            case ESource.Slider:
+                Slider slider = (Slider)source;
+                textToSync.text = slider.value.ToString();
+                break;
+            default:
+                // TextMeshProUGUI source
+                TMPro.TextMeshProUGUI text = (TMPro.TextMeshProUGUI)source;
+                if (syncText) textToSync.text = text.text;
+                if (syncFontSize) textToSync.fontSize = text.fontSize;
+                if (syncMargin) textToSync.margin = text.margin;
+                break;
         }
+    }
+
+    public enum ESource
+    {
+        TextMeshPro,
+        Slider,
     }
 }
