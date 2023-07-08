c4dec0c02f8bfdcd2a42b2dad9cfc68ccd9f7212
MrMeik <mlabarbera98a@gmail.com>
2023-07-08 11:36:04 -0400

    Add base scripts

---
+++ Assets/Scripts/Cells/IPuppet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>
+/// Interface to control an entity by a player
+/// </summary>
+public interface IPuppet
+{
+    /// <summary>
+    /// Moves puppet
+    /// </summary>
+    /// <param name="moveToward">Magnitude should be <= 1 </param>
+    public void Move(Vector2 moveDelta);
+
+    /// <summary>
+    /// Turn puppet character towards specified direction
+    /// </summary>
+    /// <param name="faceToward"></param>
+    /// <returns>Returns angle away from target after turning</returns>
+    public float FaceToward(Vector2 faceToward);
+
+    /// <summary>
+    /// Shoots in the current facing direction
+    /// </summary>
+    public void Shoot();
+
+    /// <summary>
+    /// Dashes in the current facing direction
+    /// </summary>
+    public void Dash();
+}
---
+++ Assets/Scripts/Extensions/MonobehaviorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Extensions
+{
+    public static class MonobehaviorExtensions
+    {
+        public static void EnsureHasReference<T>(this MonoBehaviour script, ref T field)
+        {
+            if (field == null)
+            {                
+                Debug.LogError($"{script.GetType().Name} script is missing field of type {typeof(T)}. Manually acquiring...");
+                field = script.GetComponent<T>() ?? throw new System.Exception("Cannot be manually acquired... loser");
+            }
+        }
+    }
+}
