2c69b3758aadddaaca91524a6b77428bcc141fa1
Kenneth Mead <kennethmead784@gmail.com>
2023-07-08 03:14:06 -0400

    Added extensions

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -4,6 +4,15 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string SceneName;
+    public bool AutoSubscribe = true;
+
+    private void Awake()
+    {
+        if (AutoSubscribe)
+        {
+            //transform.find
+        }
+    }
 
     public void GoToScene()
     {
