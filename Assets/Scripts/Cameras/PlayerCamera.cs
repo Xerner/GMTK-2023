26956932ccddaacfd7fd73e2f5a97abb1acfe8ff
Joseph Constantakis <jcon5294@gmail.com>
2023-07-09 19:08:47 -0400

    gutted cinemachine

--- Assets/Scripts/Cameras/PlayerCamera.cs
+++ Assets/Scripts/Cameras/PlayerCamera.cs
@@ -12,29 +12,37 @@ namespace Assets.Scripts.Cameras
         [Range(2, 20)]
         public float cameraTargetDivider;
 
-        [SerializeField]
-        private GameObject _focusPoint;
-        [SerializeField]
-        private Camera _mainCamera;
+        private CinemachineVirtualCamera _camera;
+
+        //[SerializeField]
+        //private GameObject _focusPoint;
+        //[SerializeField]
+        //private Camera _mainCamera;
         private Cell controlledCell;
 
         private void Start()
         {
             Player.Instance.OnCellSwap += UpdateControlledCell;
-            this.EnsureHasReference(ref _mainCamera);
-            this.EnsureHasReference(ref _focusPoint);
+            _camera = GetComponent<CinemachineVirtualCamera>();
+            //this.EnsureHasReference(ref _mainCamera);
+            //this.EnsureHasReference(ref _focusPoint);
         }
 
         public void UpdateControlledCell(Cell cell)
         {
             controlledCell = cell;
+            _camera.Follow = cell.transform;
         }
 
         void Update()
         {
-            var mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            var cameraTargetPosition = (mousePosition + (cameraTargetDivider - 1) * controlledCell.transform.position) / cameraTargetDivider;
-            _focusPoint.transform.position = cameraTargetPosition;
+            //var mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            //var cameraTargetPosition = (mousePosition + (cameraTargetDivider - 1) * controlledCell.transform.position) / cameraTargetDivider;
+            //_focusPoint.transform.position = cameraTargetPosition;
+
+            //var camPosition = _mainCamera.transform.position;
+            //camPosition.z = -10;
+            //_mainCamera.transform.position = camPosition;
         }
     }
 }
