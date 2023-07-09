da4517203a380d75ffd777eab0dc0083d6c40b7b
Kenneth Mead <kennethmead784@gmail.com>
2023-07-08 21:26:10 -0400

    Oops. Deleted the camera

--- Assets/Scripts/Cameras/PlayerCamera.cs
+++ Assets/Scripts/Cameras/PlayerCamera.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using Assets.Scripts.Cells;
+﻿using Assets.Scripts.Cells;
 using Assets.Scripts.Extensions;
 using Cinemachine;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,19 +15,22 @@ namespace Assets.Scripts.Cameras
         [SerializeField]
         private GameObject _focusPoint;
         [SerializeField]
+        private Camera _mainCamera;
+        [SerializeField]
         private Cell _playerCell;
 
         private void Start()
         {
             _mainCamera = Camera.main;
             this.EnsureHasReference(ref _playerCell);
+            this.EnsureHasReference(ref _mainCamera);
             this.EnsureHasReference(ref _focusPoint);
         }
 
         // Update is called once per frame
         void Update()
         {
-            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
             var cameraTargetPosition = (mousePosition + (cameraTargetDivider - 1) * _playerCell.transform.position) / cameraTargetDivider;
             _focusPoint.transform.position = cameraTargetPosition;
         }
